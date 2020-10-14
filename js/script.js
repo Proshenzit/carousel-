15df2a921e60f01568e87db6285bc4767de1618f
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2020-10-14 15:47:25 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// $(document).ready(function(){
+//     $('.owl-carousel').owlCarousel({
+
+// autoplay:true,
+
+// autoplaytimeout:2000,
+
+// autoplayHoverPause:true,
+// loop:true,
+// margin:10,
+
+// responsiveclcass:true,
+
+// nav:true,
+
+//     });
+//   });
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+
+ autoplaytimeout:2000,
+ autoplayHoverPause:true,
+ loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.roy owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+
+ autoplaytimeout:2000,
+ autoplayHoverPause:true,
+ loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
